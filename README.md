8a4fafb7c94d25c768ce51943e112299d6c6f8fc
akihisa-koyama <akihisa-koyama@cybozu.co.jp>
2020-07-30 19:34:17 +0900

    Add Tweet component

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # react-only-tutorial
 React だけでアプリを作るチュートリアル https://sbfl.net/blog/2019/02/20/react-only-tutorial/
+
+## How to run
+
+```
+# Run server and access index.html
+```
--- main.jsx
+++ main.jsx
@@ -1,5 +1,20 @@
 function App() {
-  return <div>Hello React!</div>
+  return (
+    <div>
+      <Tweet
+        icon="📛"
+        displayName="豆腐・オン・ファイヤー太郎"
+        accountName="tofu_on_fire"
+        content="名札です"
+      />
+      <Tweet
+        icon="🥺"
+        displayName="ぴえん"
+        accountName="pien"
+        content="記念カキコ"
+      />
+    </div>
+  )
 }
 
 const target = document.querySelector('#app');
